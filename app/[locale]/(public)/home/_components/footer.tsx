428ed5e5522b147e1c242412844ff991557ec550
kingnnt <dev.kingnnt@gmail.com>
2026-07-09 14:24:47 +0700

    refactor(home): move github mark to public/icons and render via mask

--- app/[locale]/(public)/home/_components/footer.tsx
+++ app/[locale]/(public)/home/_components/footer.tsx
@@ -84,13 +84,10 @@ export function SiteFooter() {
                     rel="noreferrer"
                     className="group inline-flex items-center gap-2 text-sm text-muted-foreground transition-colors hover:text-primary"
                   >
-                    <svg
+                    <span
                       aria-hidden
-                      viewBox="0 0 24 24"
-                      className="h-4 w-4 fill-current text-primary/70 transition-colors group-hover:text-primary"
-                    >
-                      <path d="M12 .5C5.73.5.5 5.73.5 12c0 5.08 3.29 9.39 7.86 10.91.58.11.79-.25.79-.56 0-.28-.01-1.02-.02-2-3.2.7-3.88-1.54-3.88-1.54-.52-1.33-1.28-1.68-1.28-1.68-1.05-.72.08-.7.08-.7 1.16.08 1.77 1.19 1.77 1.19 1.03 1.77 2.71 1.26 3.37.96.1-.75.4-1.26.73-1.55-2.55-.29-5.24-1.28-5.24-5.69 0-1.26.45-2.29 1.19-3.1-.12-.29-.52-1.46.11-3.05 0 0 .97-.31 3.18 1.18a11.1 11.1 0 0 1 5.79 0c2.2-1.49 3.17-1.18 3.17-1.18.63 1.59.23 2.76.11 3.05.74.81 1.19 1.84 1.19 3.1 0 4.42-2.69 5.39-5.25 5.68.41.35.78 1.05.78 2.12 0 1.53-.01 2.77-.01 3.15 0 .31.21.68.8.56A10.52 10.52 0 0 0 23.5 12C23.5 5.73 18.27.5 12 .5Z" />
-                    </svg>
+                      className="mask-icon h-4 w-4 text-primary/70 transition-colors group-hover:text-primary [mask-image:url(/icons/github.svg)] [-webkit-mask-image:url(/icons/github.svg)]"
+                    />
                     GitHub
                   </a>
                 </li>
